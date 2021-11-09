5c58e7921a3a1be36eaaae7705e7ef881de1db29
Mateusz Aliyev <82393815+mateuszaliyev@users.noreply.github.com>
2021-11-09 12:29:40 +0100

    feat: extend tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   darkMode: false,
   mode: "jit",
@@ -7,7 +9,31 @@ module.exports = {
     "./src/pages/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        error: {
+          dark: colors.red[700],
+          DEFAULT: colors.red[600],
+          light: colors.red[500],
+        },
+        info: {
+          dark: colors.sky[600],
+          DEFAULT: colors.sky[500],
+          light: colors.sky[400],
+        },
+        primary: colors.blue,
+        success: {
+          dark: colors.green[600],
+          DEFAULT: colors.green[500],
+          light: colors.green[400],
+        },
+        warning: {
+          dark: colors.amber[600],
+          DEFAULT: colors.amber[500],
+          light: colors.amber[400],
+        },
+      },
+    },
   },
   variants: {
     extend: {},
