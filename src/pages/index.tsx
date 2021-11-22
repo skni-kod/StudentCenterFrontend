279c1c681dd52bc88b80004a9736b43f44adcb28
Mateusz Aliyev <82393815+mateuszaliyev@users.noreply.github.com>
2021-11-22 22:50:24 +0100

    feat: add `useTypeSafeTranslation` hook

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,11 @@
 import type { NextPage } from "next";
-import useTranslation from "next-translate/useTranslation";
+
+import useTypeSafeTranslation from "@/hooks/useTypeSafeTranslation";
 
 const Home: NextPage = () => {
-  const { t } = useTranslation("common");
+  const { t } = useTypeSafeTranslation();
 
-  return <div className="flex text-5xl">{t("homepage")}</div>;
+  return <div className="flex text-5xl">{t("common:homepage")}</div>;
 };
 
 export default Home;
