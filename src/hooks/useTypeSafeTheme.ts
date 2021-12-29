97b928bf79e0146f53c256452ee4f62b451145f8
Mateusz Aliyev <82393815+mateuszaliyev@users.noreply.github.com>
2021-12-29 23:58:41 +0100

    feat: add `useTypeSafeTheme` hook (#51)

---
+++ src/hooks/useTypeSafeTheme.ts
@@ -0,0 +1,22 @@
+import { useTheme } from "next-themes";
+
+import type Theme from "@/types/theme";
+
+export type UseTypeSafeThemeProps = {
+  /** Forced theme name for the current page */
+  forcedTheme?: Theme;
+  /** If `enableSystem` is true and the active theme is "system", this returns whether the system preference resolved to "dark" or "light". Otherwise, identical to `theme` */
+  resolvedTheme?: Theme;
+  /** Update the theme */
+  setTheme: (theme: Theme) => void;
+  /** If enableSystem is true, returns the System theme preference ("dark" or "light"), regardless what the active theme is */
+  systemTheme?: Omit<Theme, "system">;
+  /** Active theme name */
+  theme?: Theme;
+  /** List of all available theme names */
+  themes: Theme[];
+};
+
+const useTypeSafeTheme = useTheme as () => UseTypeSafeThemeProps;
+
+export default useTypeSafeTheme;
