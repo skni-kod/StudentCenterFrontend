72b13ec55f1594830a0598386fa34fc6e7a67e82
Mateusz Aliyev <82393815+mateuszaliyev@users.noreply.github.com>
2022-01-02 21:31:50 +0100

    feat: add framer motion (#53)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,27 @@
 import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
 import { ThemeProvider } from "next-themes";
+
+import { AnimatePresence } from "framer-motion";
 
 import "@/styles/globals.scss";
 
-const App = ({ Component, pageProps }: AppProps) => (
-  <ThemeProvider attribute="class">
-    <Component {...pageProps} />
-  </ThemeProvider>
-);
+const handleExitComplete = () => {
+  if (typeof window !== "undefined") {
+    window.scrollTo({ top: 0 });
+  }
+};
+
+const App = ({ Component, pageProps }: AppProps) => {
+  const router = useRouter();
+
+  return (
+    <ThemeProvider attribute="class">
+      <AnimatePresence exitBeforeEnter onExitComplete={handleExitComplete}>
+        <Component {...pageProps} key={router.route} />
+      </AnimatePresence>
+    </ThemeProvider>
+  );
+};
 
 export default App;
