c8a21b4a11f3acc575907aacd678d23dd0a008fd
Mateusz Aliyev <82393815+mateuszaliyev@users.noreply.github.com>
2022-01-24 00:57:25 +0100

    refactor: improve i18n module (#65)

--- src/modules/i18n/hooks/use-translation.ts
+++ src/modules/i18n/hooks/use-translation.ts
@@ -1,23 +1,7 @@
-import type { TranslationQuery } from "next-translate";
 import useNextTranslateTranslation from "next-translate/useTranslation";
 
-import type I18nKey from "../types/i18n";
+import type UseTranslation from "../types/use-translation";
 
-export type I18n = {
-  lang: string;
-  t: Translate;
-};
-
-export type Translate = <T = string>(
-  i18nKey: I18nKey,
-  query?: TranslationQuery | null,
-  options?: {
-    default?: string;
-    fallback?: string[] | string;
-    returnObjects?: boolean;
-  }
-) => T;
-
-const useTranslation = (): I18n => useNextTranslateTranslation();
+const useTranslation: UseTranslation = () => useNextTranslateTranslation();
 
 export default useTranslation;
---
+++ src/modules/i18n/types/use-translation.ts
@@ -0,0 +1,5 @@
+import type I18n from "./i18n";
+
+type UseTranslation = () => I18n;
+
+export default UseTranslation;
