9ab59df2b5043e1f92b1669c1b12ed974ee44314
Mateusz Aliyev <82393815+mateuszaliyev@users.noreply.github.com>
2022-02-19 17:30:20 +0100

    refactor: generate i18n types with next-translate-ts (#73)

--- src/modules/i18n/hooks/use-translation.ts
+++
@@ -1,8 +0,0 @@
-import useNextTranslateTranslation from "next-translate/useTranslation";
-
-import type UseTranslation from "../types/use-translation";
-
-/** Recommended way to use translations in pages / components. */
-const useTranslation: UseTranslation = () => useNextTranslateTranslation();
-
-export default useTranslation;
--- src/modules/i18n/types/use-translation.ts
+++
@@ -1,5 +0,0 @@
-import type I18n from "./i18n";
-
-type UseTranslation = () => I18n;
-
-export default UseTranslation;
