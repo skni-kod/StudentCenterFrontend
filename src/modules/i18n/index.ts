e6c9205335c2da6db7f5578b49c3dc25943573d6
Mateusz Aliyev <82393815+mateuszaliyev@users.noreply.github.com>
2022-02-19 17:41:24 +0100

    fix: stop tracking i18n `index.ts` file

--- src/modules/i18n/index.ts
+++
@@ -1,8 +0,0 @@
-import useTranslation from "./hooks/use-translation";
-import type I18n from "./types/i18n";
-import type I18nKey from "./types/i18n-key";
-import type Translate from "./types/translate";
-import type UseTranslation from "./types/use-translation";
-
-export type { I18n, I18nKey, Translate, UseTranslation };
-export { useTranslation };
